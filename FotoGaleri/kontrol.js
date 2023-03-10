b062736cc2c6c07e521d19ae991c8237dd3f9c4e
erhanyagmur <119954665+erhanyagmur@users.noreply.github.com>
2023-03-11 00:51:12 +0300

    Add files via upload

---
+++ FotoGaleri/kontrol.js
@@ -0,0 +1,44 @@
+function yukle()
+{
+	document.getElementsByTagName("body")[0].style.backgroundColor="black";
+}
+function resmigoster1()
+{
+	document.getElementById("resim1").style.zIndex=2;
+	document.getElementById("resim2").style.zIndex=1;
+	document.getElementById("resim3").style.zIndex=1;
+	document.getElementById("resim4").style.zIndex=1;
+	document.getElementById("resim5").style.zIndex=1;	
+}
+function resmigoster2()
+{
+	document.getElementById("resim1").style.zIndex=1;
+	document.getElementById("resim2").style.zIndex=2;
+	document.getElementById("resim3").style.zIndex=1;
+	document.getElementById("resim4").style.zIndex=1;
+	document.getElementById("resim5").style.zIndex=1;	
+}
+function resmigoster3()
+{
+	document.getElementById("resim1").style.zIndex=1;
+	document.getElementById("resim2").style.zIndex=1;
+	document.getElementById("resim3").style.zIndex=2;
+	document.getElementById("resim4").style.zIndex=1;
+	document.getElementById("resim5").style.zIndex=1;	
+}
+function resmigoster4()
+{
+	document.getElementById("resim1").style.zIndex=1;
+	document.getElementById("resim2").style.zIndex=1;
+	document.getElementById("resim3").style.zIndex=1;
+	document.getElementById("resim4").style.zIndex=2;
+	document.getElementById("resim5").style.zIndex=1;	
+}
+function resmigoster5()
+{
+	document.getElementById("resim1").style.zIndex=1;
+	document.getElementById("resim2").style.zIndex=1;
+	document.getElementById("resim3").style.zIndex=1;
+	document.getElementById("resim4").style.zIndex=1;
+	document.getElementById("resim5").style.zIndex=2;	
+}
